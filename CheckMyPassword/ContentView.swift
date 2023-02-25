7c987c0cb2d99fa12515ed2002814cfed358ba1c
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-02-25 19:42:18 +0700

    finale

--- CheckMyPassword/ContentView.swift
+++ CheckMyPassword/ContentView.swift
@@ -5,17 +5,94 @@
 //  Created by Putut Yusri Bahtiar on 25/02/23.
 //
 
+import CommonCrypto
 import SwiftUI
 
 struct ContentView: View {
+    @State private var password = ""
+    @State private var isPasswordInList = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Image("lock")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .padding(.bottom, 30)
+            
+            Text("Check My Password")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+            
+            Text("Enter your password to check if it has been leaked in a data breach.")
+                .font(.headline)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.gray)
+                .padding()
+            
+            TextField("Password", text: $password)
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+                .padding(.horizontal, 20)
+                .padding(.bottom, 30)
+                .autocapitalization(.none)
+            
+            Button(action: {
+                isPasswordInList = checkPasswordInList(password)
+            }) {
+                Text("Check Password")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding(.horizontal, 20)
+            }
+            
+            if let resultText = resultText {
+                Text(resultText)
+                    .font(.headline)
+                    .padding(.top, 30)
+                    .foregroundColor(isPasswordInList ? .red : .green)
+            }
+            
+            Spacer()
         }
-        .padding()
+        .padding(.horizontal, 20)
+    }
+    
+    var resultText: String? {
+        if password.isEmpty {
+            return nil
+        }
+        
+        if isPasswordInList {
+            return "Your password has been found in a data breach."
+        } else {
+            return "Your password has not been found in any data breaches."
+        }
+    }
+    
+    func checkPasswordInList(_ password: String) -> Bool {
+        guard let fileURL = Bundle.main.url(forResource: "passwordlist", withExtension: "txt") else {
+            print("Password list file not found!")
+            return false
+        }
+        
+        let passwordList = try! String(contentsOf: fileURL, encoding: .utf8)
+        return passwordList.contains(password)
+    }
+    
+    func LoadPasswordsFromFile() -> [String]{
+        let fileURL = Bundle.main.url(forResource: "passwordlist", withExtension: "txt")!
+        let contents = try! String(contentsOf: fileURL, encoding: .utf8)
+        let passwordList = contents.split(separator: "\n").map(String.init)
+        return passwordList
     }
 }
 
