a4f2485c85cfcdb979d29013c1bdcd59a6e08cdf
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-02-25 18:44:09 +0700

    Initial Commit

---
+++ CheckMyPassword/CheckMyPasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMyPasswordApp.swift
+//  CheckMyPassword
+//
+//  Created by Putut Yusri Bahtiar on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckMyPasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
